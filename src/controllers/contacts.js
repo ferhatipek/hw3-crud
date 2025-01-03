861a9d5845895f40f8e29d96b3894f865b6e9aee
YNazymko12 <yulychka12@gmail.com>
2025-01-03 12:53:57 +0100

    refactoring

---
+++ src/controllers/contacts.js
@@ -0,0 +1,29 @@
+import createHttpError from 'http-errors';
+
+import * as contactServices from '../services/contacts.js';
+
+export const getContactsController = async (req, res) => {
+  const data = await contactServices.getContacts();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data,
+  });
+};
+
+export const getContactsByIdController = async (req, res) => {
+  const { id } = req.params;
+
+  const data = await contactServices.getContactById(id);
+
+  if (!data) {
+    throw createHttpError(404, `Contact with id=${id} not found`);
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${id}!`,
+    data,
+  });
+};
